d4997831a1e7956ce4d42196348c3ad8ac216103
N A V E E N <162016882+NaveenTechist@users.noreply.github.com>
2025-10-15 20:34:22 +0530

    Create (ASKED)Sorting in Increasing Order.cpp

---
+++ Sorting/Bubble-Sort/(ASKED)Sorting in Increasing Order.cpp
@@ -0,0 +1,21 @@
+// Asked in hiring 10+ companies recently
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution{
+    public:
+    void bubbleSort(int arr[], int n){
+        for(int i = n-1; i >= 1; i--){
+            bool is_true  = true;
+            for(int j = 0; j <= i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    is_true = false;
+                }
+            }
+            if(is_true) break;
+        }
+    }
+};
