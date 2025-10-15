dd151008a5ec0fa667891b81d73d9fd526d26c4d
N A V E E N <162016882+NaveenTechist@users.noreply.github.com>
2025-10-15 22:50:02 +0530

    Create (ASKED)Sorting Array of Integers in Increasing Order.cpp

---
+++ Sorting/Insertion-Sort/(ASKED)Sorting Array of Integers in Increasing Order.cpp
@@ -0,0 +1,23 @@
+// Companies
+// Amazon
+// Google
+// Microsoft
+// Bloomberg
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution{
+    public:
+    void insertionSort(int arr[], int n){
+        for(int i = 1; i <= n-1; i++){
+            int j = i;
+            while(arr[j-1] > arr[j] && j >= 1 ){
+                int temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+    }
+};
