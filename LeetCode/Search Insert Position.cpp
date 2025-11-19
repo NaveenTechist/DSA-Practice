c0cede12fdf48a6fddc0230cd82fb19292bce3ff
N A V E E N <162016882+NaveenTechist@users.noreply.github.com>
2025-11-19 22:15:37 +0530

    Create Search Insert Position.cpp

---
+++ LeetCode/Search Insert Position.cpp
@@ -0,0 +1,31 @@
+
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] >= target){
+                return i;
+            }
+            if( i == nums.size()-1) return i+1;
+        }
+        return -1;
+    }
+};
+
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+  
+Example 2:
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+  
+Example 3:
+Input: nums = [1,3,5,6], target = 7
+Output: 4
