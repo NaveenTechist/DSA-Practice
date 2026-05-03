10158ac5db5555fd9e5a3f0c81511f4438121296
N A V E E N <162016882+NaveenTechist@users.noreply.github.com>
2026-05-04 04:39:50 +0530

    Update Two Sum.py

--- Arrays/Two Sum.py
+++ Arrays/Two Sum.py
@@ -1,7 +1,7 @@
 arr = [2,7,11,15] 
 target = 9
 di = {}
-for i, num in enumerate(arr) :
+for i, num in enumerate(arr) : # (enumrate) gave index and value
      if(target - num in di):
          print([di[target-num], i])
      di[num] = i
